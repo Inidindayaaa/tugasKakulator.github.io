3692a73c292480a449242db25e08887e934792dc
Dinda Hanifah Hasna <118790668+Inidindayaaa@users.noreply.github.com>
2023-03-14 10:57:11 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+const operasi = (prompt("masukan oprator"));
+const angka1 = parseInt(prompt("masukan angka 1"));
+const angka2= parseInt(prompt("masukan angka 2"));
+
+if(operasi == "+"){
+    let tambah = angka1 + angka2;
+    console.log(tambah);
+}else if(operasi == "-"){
+    let kurang = angka1 - angka2;
+    console.log(kurang);
+}else if(operasi == "*"){
+    let kali = angka1 * angka2;
+    console.log(kali);
+}else{
+    let bagi = angka1 / angka2;
+    console.log(bagi);
+}
+
+
+
+
